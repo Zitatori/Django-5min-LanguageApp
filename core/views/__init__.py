30deecfd861ec02078cb56adc62b691ee05b342a
Zitatori <zitatori@gmail.com>
2026-05-20 16:54:26 +0200

    Add tutor_match_status view

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,5 +1,5 @@
 from .home import home
 from .student import create_request, request_detail, create_interview_request, student_history
-from .tutor import tutor_dashboard
+from .tutor import tutor_dashboard, tutor_match_status
 from .lesson import lesson_room, video_room, VideoRoomView
 from .auth import signup
