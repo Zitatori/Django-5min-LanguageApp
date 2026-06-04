e41f6ed854df713d44f683e92bb31e8d7da7ce3b
Zitatori <zitatori@gmail.com>
2026-06-04 18:11:54 +0200

    Remove language selection from signup; languages set by admin after registration

--- core/views/auth.py
+++ core/views/auth.py
@@ -2,15 +2,13 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
-from core.models import StudentProfile, TutorProfile, LessonLanguage
+from core.models import StudentProfile, TutorProfile
 from core.models import PointBalance, PointTransaction
 
 SIGNUP_BONUS_POINTS = 10
 
 
 def signup(request):
-    languages = LessonLanguage.objects.all()
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
 
@@ -20,12 +18,10 @@ def signup(request):
             StudentProfile.objects.get_or_create(user=user)
 
             wants_to_teach = request.POST.get("wants_to_teach")
-            selected_languages = request.POST.getlist("tutor_languages")
 
             if wants_to_teach:
-                tutor_profile, _ = TutorProfile.objects.get_or_create(user=user)
-                tutor_profile.languages.set(selected_languages)
-                tutor_profile.save()
+                TutorProfile.objects.get_or_create(user=user)
+                # 言語はサインアップ後に管理者が設定
 
             # 新規ユーザーにサインアップボーナスを付与
             PointBalance.objects.create(user=user, balance=SIGNUP_BONUS_POINTS)
@@ -43,5 +39,4 @@ def signup(request):
 
     return render(request, "registration/signup.html", {
         "form": form,
-        "languages": languages,
     })
