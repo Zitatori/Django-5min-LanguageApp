45f596cf8b5039ba1a5a5d3e4f058aa84e368527
Zitatori <zitatori@gmail.com>
2026-07-13 09:23:26 +0200

    Fix missing request_admin_chat export in views/__init__.py

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,5 +1,5 @@
 from .home import home, privacy
-from .student import create_request, request_detail, create_interview_request, student_history, student_online_counts
+from .student import create_request, request_detail, create_interview_request, student_history, student_online_counts, request_admin_chat
 from .tutor import tutor_dashboard, tutor_match_status, tutor_set_offline
 from .lesson import lesson_room, lesson_end, lesson_rating, video_room, VideoRoomView
 from .auth import signup
